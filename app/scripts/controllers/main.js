504548c159ebdc61953917c25c7f075a9f0dd873
Tom <tomrhernandez@gmail.com>
2016-01-13 21:51:10 +0000

    Add made todo list sortable. Added localStorage service for data persistence.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,22 @@
  * Controller of the workspaceApp
  */
 angular.module('workspaceApp')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MainCtrl', function ($scope, localStorageService) {
+    var todosInStore = localStorageService.get('todos');
+    
+    $scope.todos = todosInStore || [];
+    $scope.$watch('todos', function() {
+      localStorageService.set('todos', $scope.todos);
+    }, true);
+    
+    //Fix later to error check blank entries and duplicate entries
+    $scope.addTodo = function() {
+      $scope.todos.push($scope.todo);
+      $scope.todo = '';
+    };
+    
+    $scope.removeTodo = function(index) {
+      $scope.todos.splice(index, 1);
+    };
+    
   });
